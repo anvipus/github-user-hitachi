6d3dc7f3835920713496d31dcb20eda9eadf7a52
Seno Adji <seno.adji@iseller.id>
2024-09-02 15:32:58 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.jetbrains.kotlin.kapt)
     alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.navigation.plugin)
 }
 
 android {
